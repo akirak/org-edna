c940b8e164cc8e1de43522142576ebb24d735ba5
Radon Rosborough <radon.neon@gmail.com>
2018-07-11 22:06:36 -0600

    Update org-edna
    Timestamp: 2018-07-11 22:04:56
    GNU ELPA commit: d9d04d151c636b4836d3789c445dc3cdf95621d4
    Emacs commit: 84e5986902c7d7274f438c48c82949436eb9093d

---
+++ test.mk
@@ -0,0 +1,28 @@
+# Copyright (C) 2017-2018 Free Software Foundation, Inc.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# EDE only allows arbitrary code from an external makefile, so this is how we've
+# got to do testing.
+
+test: compile
+	@$(EMACS) \
+	$(EMACSFLAGS) \
+	$(addprefix -L ,$(LOADPATH)) \
+	-L "." \
+	-l "ert" \
+	-l "org-edna-tests.el" \
+	-f ert-run-tests-batch-and-exit
+
+include Makefile
